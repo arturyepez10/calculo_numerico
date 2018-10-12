886e2d162a44fdd94a498d47e3a58f0c597c3369
Arturo Yepez <arturyepez10@gmail.com>
2018-10-11 22:27:36 -0400

    Añadido el codigo de los metodos de Sustitucion hacia Atras y hacia Adelante

--- GAUSS.m
+++ GAUSS.m
@@ -6,9 +6,9 @@ function [A b] = GAUSS(A, b)
     for i = k+1:1:n
       alpha = A(i,k)/A(k,k);
       for j = k:1:n
-        A(i,j) = A(i,j) - (alpha*A(k,j));
+        A(i,j) = A(i,j) - (alpha * A(k,j));
       end
-      b(i) = b(i) - (alpha*b(k));
+      b(i) = b(i) - (alpha * b(k));
     end
   end
 endfunction
---
+++ diagonal.m
@@ -0,0 +1,9 @@
+# diagonal
+
+function [x] = diagonal(A, b)
+  n = length(A);
+  x = nan * ones(n,1);
+  for i = 1:1:n
+    x(i) = b(i)/A(i,i);
+  end
+endfunction
