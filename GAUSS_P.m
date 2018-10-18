dbd68096e9531c5d555a56995edf7d19dac59e21
Arturo Yepez <arturyepez10@gmail.com>
2018-10-17 21:41:04 -0400

    se añaden funciones de gauss con pivoteo, descomposición LU, inversa mediante descomposición LU y matriz de toeplitz

---
+++ GAUSS_P.m
@@ -0,0 +1,36 @@
+# METODO DE GAUSS CON PIVOTEO
+
+function [A b] = GAUSS_P(A,b)
+  [m, n] = size(A);
+  for k=1:1:n-1
+    maximo=abs(A(k,k));
+    imax=k;
+    
+    for i=k+1:1:n
+        if abs(A(i,k))>maximo
+            maximo=abs(A(i,k));
+            imax=i;
+        end
+    end
+    
+    for j=k:1:n
+        temp= A(k,j);
+        A(k,j)=A(imax,j);
+        A(imax,j)=temp;
+    end
+    
+    temp=b(k);
+    b(k)= b(imax);
+    b(imax)=temp;
+    
+    
+    for i=k+1:1:n
+        alpha=A(i,k)/A(k,k);
+        for j=k:1:n
+            A(i,j)=A(i,j)-alpha*A(k,j);
+            
+        end
+        b(i)=b(i)-alpha*b(k);
+    end
+  end
+endfunction
---
+++ sistemaLU.m
@@ -0,0 +1,11 @@
+#
+
+function x = sistemaLU(A,b)
+    n = length(A);
+    Lu = LU(A);
+    L = tril(Lu,-1) + eye(n);
+    U = triu(Lu);
+    
+    y = sustad(L,b);
+    x = sustat(U,y); 
+endfunction
