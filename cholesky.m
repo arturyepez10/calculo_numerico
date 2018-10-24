e950b16c59c731181c59b4bca713aac4df169ad0
Arturo Yepez <arturyepez10@gmail.com>
2018-10-24 11:09:57 -0400

    Se arregla error en cholesky

--- cholesky.m
+++ cholesky.m
@@ -14,7 +14,7 @@ function L = cholesky(A)
       for m=1:1:k-1
         suma2 = suma2 + L(i,m)*L(k,m);
       end
-      L(i,k) = (A(i,k) - suma2)^(1/2)
+      L(i,k) = (A(i,k) - suma2)/L(k,k);
     end
   end
 end
