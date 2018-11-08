0d8ff8bae477bc4a597d52df44a50f946f532aa5
Arturo Yepez <arturyepez10@gmail.com>
2018-11-07 20:39:50 -0400

    se añade algoritmo de gauss_seidel, iterativo

--- prueba.m
+++ prueba.m
@@ -3,6 +3,6 @@ matriz_prueba = Hilbert(11) + 1.025*eye(11)
 x = ones(11,1)
 b = matriz_prueba*x
 t = cputime;
-[xn k] = jacobi(matriz_prueba,b,0*ones(11,1), 1500, 10^-7)
+[xn k] = gauss_seidel(matriz_prueba,b,0*ones(11,1), 1500, 10^(-7))
 
 disp(cputime-t)
