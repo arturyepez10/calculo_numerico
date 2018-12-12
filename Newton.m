17f5abfd7a7489e3c7ae7ffeb799a1a2575f3fad
invitado <invitado@a7.ac.labf.usb.ve>
2018-12-12 13:05:11 -0500

    Polinomio interpolante de Newton

---
+++ Newton.m
@@ -0,0 +1,11 @@
+# POLINOMIO INTERPOLANTE DE NEWTON
+
+function c = Newton(X,Y)
+  c = Y;
+  n = length(X);
+  for j=2:n
+    for i= n:-1:j
+      c(i) = (c(i)-c(i-1))/(X(i)-X(i-j+1))
+    end
+  end
+end
